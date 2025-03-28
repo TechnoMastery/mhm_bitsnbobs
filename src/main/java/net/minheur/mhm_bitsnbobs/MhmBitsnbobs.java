805fcaa348e895f608c63f91203d2a4ca738e09f
Minheur <adrien.rousselet@hotmail.com>
2025-03-28 19:29:28 +0100

    setup #1

--- src/main/java/net/minheur/mhm_bitsnbobs/MhmBitsnbobs.java
+++ src/main/java/net/minheur/mhm_bitsnbobs/MhmBitsnbobs.java
@@ -1,6 +1,7 @@
 package net.minheur.mhm_bitsnbobs;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -8,35 +9,32 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minheur.mhm_bitsnbobs.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(mhmBitsnbobs.MOD_ID)
-public class mhmBitsnbobs
+@Mod(MhmBitsnbobs.MOD_ID)
+public class MhmBitsnbobs
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "mhm_bitsnbobs";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public mhmBitsnbobs(FMLJavaModLoadingContext context)
+    public MhmBitsnbobs(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -45,9 +43,11 @@ public class mhmBitsnbobs
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+             event.accept(ModItems.SAPPHIRE);
+             event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/minheur/mhm_bitsnbobs/item/ModCreativeModTabs.java
@@ -0,0 +1,9 @@
+package net.minheur.mhm_bitsnbobs.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.registries.DeferredRegister;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = (13:56)
+
+}
