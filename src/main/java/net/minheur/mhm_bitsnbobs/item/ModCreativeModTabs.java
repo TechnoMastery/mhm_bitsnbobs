e669650261a5435d5d50fd95c27eb33ce651c012
Minheur <adrien.rousselet@hotmail.com>
2025-03-29 11:05:15 +0100

    setup #2

--- src/main/java/net/minheur/mhm_bitsnbobs/item/ModCreativeModTabs.java
+++ src/main/java/net/minheur/mhm_bitsnbobs/item/ModCreativeModTabs.java
@@ -1,9 +1,31 @@
 package net.minheur.mhm_bitsnbobs.item;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.minheur.mhm_bitsnbobs.MhmBitsnbobs;
 
 public class ModCreativeModTabs {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = (13:56)
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MhmBitsnbobs.MOD_ID);
 
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.RAW_SAPPHIRE.get());
+                        output.accept(ModItems.SAPPHIRE.get());
+
+                        output.accept(Items.DIAMOND);
+                    })
+                    .build());
+
+            public static void register(IEventBus eventBus) {
+                CREATIVE_MODE_TABS.register(eventBus);
+            }
 }
