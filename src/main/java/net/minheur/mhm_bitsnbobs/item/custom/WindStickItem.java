7349059d55e7c9320eabac3c57befc352da4ad18
benny232-232 <benjamincailliez2303@gmail.com>
2025-05-10 11:52:05 +0200

    added wind stick 1.5

---
+++ src/main/java/net/minheur/mhm_bitsnbobs/item/custom/WindStickItem.java
@@ -0,0 +1,49 @@
+package net.minheur.mhm_bitsnbobs.item.custom;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
+import net.minheur.mhm_bitsnbobs.procedures.WindStickOnUse;
+
+public class WindStickItem extends Item {
+    public WindStickItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public boolean isFoil(ItemStack pStack) {
+        return true;
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.BOW;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 60;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        InteractionResultHolder<ItemStack> ar = super.use(pLevel, pPlayer, pUsedHand);
+        pPlayer.startUsingItem(pUsedHand);
+        return ar;
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+        ItemStack retval = super.finishUsingItem(pStack, pLevel, pLivingEntity);
+        double x = pLivingEntity.getX();
+        double y = pLivingEntity.getY();
+        double z = pLivingEntity.getZ();
+        WindStickOnUse.execute(pLevel, x, y, z, pLivingEntity);
+        return retval;
+    }
+}
---
+++ src/main/java/net/minheur/mhm_bitsnbobs/procedures/WindStickOnUse.java
@@ -0,0 +1,10 @@
+package net.minheur.mhm_bitsnbobs.procedures;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelAccessor;
+
+public class WindStickOnUse {
+    public static void execute(LevelAccessor pLevel, double x, double y, double z, Entity pLivingEntity) {
+
+    }
+}
