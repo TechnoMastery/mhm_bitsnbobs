1bd57799f04d917500c4f6ad3765dd321df96b18
Minheur2000 <adrien.rousselet@hotmail.com>
2025-05-27 16:14:19 +0200

    setup 112 : removed useless import

--- src/main/java/net/minheur/mhm_bitsnbobs/MhmBitsnbobs.java
+++ src/main/java/net/minheur/mhm_bitsnbobs/MhmBitsnbobs.java
@@ -14,7 +14,6 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
