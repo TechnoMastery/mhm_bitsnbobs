81692c1bb9f6c603b55defde0c2da5dc3890df04
Minheur2000 <adrien.rousselet@hotmail.com>
2025-08-16 14:42:46 +0200

    added OtherModItem

---
+++ src/main/java/net/minheur/mhm_bitsnbobs/compat/OtherModItems.java
@@ -0,0 +1,14 @@
+package net.minheur.mhm_bitsnbobs.compat;
+
+import net.minheur.mhm_bitsnbobs.compat.compatItemlike.OtherModItem;
+
+/**
+ * This class contains the creation of {@link OtherModItem} instances.
+ * <p> It is used to reference them.
+ */
+public class OtherModItems {
+    public static final OtherModItem CALCULATION_PROCESSOR =
+            new OtherModItem("ae2", "calculation_processor");
+    public static final OtherModItem ACCUMULATION_PROCESSOR =
+            new OtherModItem("megacells", "accumulation_processor");
+}
