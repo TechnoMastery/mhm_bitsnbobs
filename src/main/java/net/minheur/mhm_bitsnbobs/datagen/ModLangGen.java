5bacb15b6ccbd00f872c7aa3b1623e1b78328886
Minheur2000 <adrien.rousselet@hotmail.com>
2025-08-30 19:06:41 +0200

    added blocks translations in datagen

--- src/main/java/net/minheur/mhm_bitsnbobs/datagen/ModLangGen.java
+++ src/main/java/net/minheur/mhm_bitsnbobs/datagen/ModLangGen.java
@@ -2,6 +2,7 @@ package net.minheur.mhm_bitsnbobs.datagen;
 
 import net.minecraft.data.PackOutput;
 import net.minheur.mhm_bitsnbobs.MhmBitsnbobs;
+import net.minheur.mhm_bitsnbobs.block.ModBlocks;
 import net.minheur.mhm_bitsnbobs.effects.ModEffects;
 import net.minheur.mhm_bitsnbobs.item.ModCreativeModTabs;
 import net.minheur.mhm_bitsnbobs.util.ModDamageTypes;
@@ -19,10 +20,12 @@ public class ModLangGen extends LanguageGenProvider {
 
     @Override
     protected void addTranslation() {
+        // credits
         addTooltipCredit("zombie_arm")
                 .en_us("Made by Jema")
                 .fr_fr("Créer par Jema");
 
+        // tooltips catalyzers
         addTooltip("catalyzer-wind_charged")
                 .en_us("This catalyzer can handle incubations that need the power of the nature.")
                 .fr_fr("Ce catalyseur peut gérer des incubations qui demandent la puissance de la nature.");
@@ -42,6 +45,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("This catalyzer can handle advanced incubations.")
                 .fr_fr("Ce catalyseur peut gérer des incubations avancées.");
 
+        // tooltip
         addTooltip("rotten_item")
                 .en_us("This item is disabled : insall \"createdieselgenerators\" to enable it.")
                 .fr_fr("Cet objet est désactivé : installez \"createdieselgenerators\" pour l'activer.");
@@ -73,6 +77,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Useless like this, but needed to craft the best sword ever!")
                 .fr_fr("Inutile comme sa, mais nécessaire pour fabriquer la mailleur épée !");
 
+        // sounds
         addSound("the_world_of_cubes")
                 .en_us("Playing the world of cubes")
                 .fr_fr("En train de jouer : The world of cubes");
@@ -122,6 +127,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Playing Ballad of the blocks")
                 .fr_fr("En train de jouer : Ballad of the blocks");
 
+        // recipes
         addRecipeName("mysterious_magic")
                 .en_us("Mysterious magic")
                 .fr_fr("Magie mystérieuse");
@@ -141,6 +147,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Atomical stabilization")
                 .fr_fr("Stabilisation atomique");
 
+        // menu (modpack)
         addMenuName("discord")
                 .en_us("Our discord")
                 .fr_fr("Notre discord");
@@ -155,10 +162,12 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Mysterious magic")
                 .fr_fr("Magie mystérieuse");
 
+        // villager
         addVillagerType("soundmaster")
                 .en_us("Sound master")
                 .fr_fr("Ingénieur son");
 
+        // effects
         addEffect(ModEffects.ICED)
                 .en_us("Iced")
                 .fr_fr("Glacé");
@@ -166,6 +175,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Quantum chocked")
                 .fr_fr("Choc quantique");
 
+        // death
         addAttackDeath(ModDamageTypes.BRAIN_FREEZE_ID.getPath())
                 .en_us("%1$s got brain-freezed")
                 .fr_fr("%1$s a eu un mal de tête givré");
@@ -185,6 +195,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("%1$s got electrocuted whilst trying to escape %2$s")
                 .fr_fr("%1$s a été éléctrocuté en essayant de fuir %2$s");
 
+        // creative mod tab
         addCreativeTab(ModCreativeModTabs.BALLS.get())
                 .en_us("Balls")
                 .fr_fr("Balles");
@@ -206,6 +217,131 @@ public class ModLangGen extends LanguageGenProvider {
         addCreativeTab(ModCreativeModTabs.MUSIC_DISCS.get())
                 .en_us("Music discs")
                 .fr_fr("Disques de musiques");
+
+        // blocks
+        addBlock(ModBlocks.ATOMICAL_STABILIZATOR)
+                .en_us("Atomical stabilizator")
+                .fr_fr("Stabilisateur atomique");
+        addBlock(ModBlocks.CATMINT)
+                .en_us("Catmint")
+                .fr_fr("Herbe a chat");
+        addBlock(ModBlocks.COMPRESSED_DIRT)
+                .en_us("Compressed dirt")
+                .fr_fr("Terre compressée");
+        addBlock(ModBlocks.CREATIVE_BLOCK)
+                .en_us("Creative block")
+                .fr_fr("Bloc créatif");
+        addBlock(ModBlocks.CREATIVE_RESIDUE_BLOCK)
+                .en_us("Block of creative residue")
+                .fr_fr("Bloc de résidu créatif");
+        addBlock(ModBlocks.DARK_HANGING_SIGN)
+                .en_us("Dark hanging sign")
+                .fr_fr("Panneau suspendu sombre");
+        addBlock(ModBlocks.DARK_LEAVES)
+                .en_us("Dark leaves")
+                .fr_fr("Feuillage sombre");
+        addBlock(ModBlocks.DARK_LOG)
+                .en_us("Dark log")
+                .fr_fr("Tronc sombre");
+        addBlock(ModBlocks.DARK_PLANKS)
+                .en_us("Dark planks")
+                .fr_fr("Planches sombre");
+        addBlock(ModBlocks.DARK_SAPLING)
+                .en_us("Dark sapling")
+                .fr_fr("Pousse d'arbre sombre");
+        addBlock(ModBlocks.DARK_SIGN)
+                .en_us("Dark sign")
+                .fr_fr("Panneau sombre");
+        addBlock(ModBlocks.DARK_WOOD)
+                .en_us("Dark wood")
+                .fr_fr("Bois sombre");
+        addBlock(ModBlocks.DEEPSLATE_SAPPHIRE_ORE)
+                .en_us("Deepslate sapphire ore")
+                .fr_fr("Minerais de saphir de pierre des abîmes");
+        addBlock(ModBlocks.DICE_BLOCK)
+                .en_us("Dice")
+                .fr_fr("Dée");
+        addBlock(ModBlocks.END_SAPPHIRE_ORE)
+                .en_us("End sapphire ore")
+                .fr_fr("Minerais de saphir de l'end");
+        addBlock(ModBlocks.EXTREMELY_DRY_DIRT_BLOCK)
+                .en_us("Extremly dry dirt block")
+                .fr_fr("Bloc de terre extrêmement sèche");
+        addBlock(ModBlocks.FREEZER)
+                .en_us("Freezer")
+                .fr_fr("Congélateur");
+        addBlock(ModBlocks.GEM_POLISHING_STATION)
+                .en_us("Gem polishing station")
+                .fr_fr("Polisseur de gems");
+        addBlock(ModBlocks.INCUBATOR)
+                .en_us("Incubator")
+                .fr_fr("Incubateur");
+        addBlock(ModBlocks.MYSTERIOUS_ALTAR)
+                .en_us("Mysterious altar")
+                .fr_fr("Autel mystérieux");
+        addBlock(ModBlocks.NETHER_SAPPHIRE_ORE)
+                .en_us("Nether sapphire ore")
+                .fr_fr("Minerais de saphir du nether");
+        addBlock(ModBlocks.RAW_SAPPHIRE_BLOCK)
+                .en_us("Block of raw sapphire")
+                .fr_fr("Bloc de saphir brute");
+        addBlock(ModBlocks.RED_CLAY)
+                .en_us("Red clay")
+                .fr_fr("Argile rouge");
+        addBlock(ModBlocks.RESOURCE_DIRT_BLOCK)
+                .en_us("Resource dirt block")
+                .fr_fr("Bloc de terre de ressource");
+        addBlock(ModBlocks.RUBINIUM_BLOCK)
+                .en_us("Rubinium block")
+                .fr_fr("Bloc de rubinium");
+        addBlock(ModBlocks.RUBIS_ORE)
+                .en_us("Rubis ore")
+                .fr_fr("Minerais de rubis");
+        addBlock(ModBlocks.SAPPHIRE_BLOCK)
+                .en_us("Block of sapphire")
+                .fr_fr("Bloc de saphir");
+        addBlock(ModBlocks.SAPPHIRE_BUTTON)
+                .en_us("Sapphire button")
+                .fr_fr("Bouton de saphir");
+        addBlock(ModBlocks.SAPPHIRE_DOOR)
+                .en_us("Sapphire door")
+                .fr_fr("Porte en saphir");
+        addBlock(ModBlocks.SAPPHIRE_FENCE)
+                .en_us("Sapphire fence")
+                .fr_fr("Barrière en saphir");
+        addBlock(ModBlocks.SAPPHIRE_FENCE_GATE)
+                .en_us("Sapphire fence gate")
+                .fr_fr("Portillon en saphir");
+        addBlock(ModBlocks.SAPPHIRE_ORE)
+                .en_us("Sapphire ore")
+                .fr_fr("Minerais de saphir");
+        addBlock(ModBlocks.SAPPHIRE_PRESSURE_PLATE)
+                .en_us("Sapphire pressure plate")
+                .fr_fr("Plaque de pression en saphir");
+        addBlock(ModBlocks.SAPPHIRE_SLAB)
+                .en_us("Sapphire slab")
+                .fr_fr("Dalle en saphir");
+        addBlock(ModBlocks.SAPPHIRE_STAIRS)
+                .en_us("Sapphire stairs")
+                .fr_fr("Escaliers en saphir");
+        addBlock(ModBlocks.SAPPHIRE_TRAPDOOR)
+                .en_us("Sapphire trapdoor")
+                .fr_fr("Trappe en saphir");
+        addBlock(ModBlocks.SAPPHIRE_WALL)
+                .en_us("Sapphire wall")
+                .fr_fr("Muret en saphir");
+        addBlock(ModBlocks.SOUND_BLOCK)
+                .en_us("Sound block")
+                .fr_fr("Bloc de musique");
+        addBlock(ModBlocks.STRIPPED_DARK_LOG)
+                .en_us("Stripped dark log")
+                .fr_fr("Tronc sombre écorcé");
+        addBlock(ModBlocks.STRIPPED_DARK_WOOD)
+                .en_us("Stripped dark wood")
+                .fr_fr("Bois sombre écorcé");
+        addBlock(ModBlocks.SUPER_CHARGED_BLOCK)
+                .en_us("Super charged block")
+                .fr_fr("Bloc super chargé");
     }
 
     public TranslationBuilder addMenuName(String id) {
