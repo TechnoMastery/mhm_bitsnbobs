540d7eabe8cc7f8892d286f0af92ef63bb5b415b
Minheur2000 <adrien.rousselet@hotmail.com>
2025-08-31 15:42:33 +0200

    worked on langs

--- src/main/java/net/minheur/mhm_bitsnbobs/datagen/ModLangGen.java
+++ src/main/java/net/minheur/mhm_bitsnbobs/datagen/ModLangGen.java
@@ -1,13 +1,19 @@
 package net.minheur.mhm_bitsnbobs.datagen;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.RecordItem;
 import net.minheur.mhm_bitsnbobs.MhmBitsnbobs;
 import net.minheur.mhm_bitsnbobs.advancement.ModAdvancements;
 import net.minheur.mhm_bitsnbobs.block.ModBlocks;
 import net.minheur.mhm_bitsnbobs.effects.ModEffects;
 import net.minheur.mhm_bitsnbobs.item.ModCreativeModTabs;
+import net.minheur.mhm_bitsnbobs.item.ModItems;
 import net.minheur.mhm_bitsnbobs.util.ModDamageTypes;
 import net.minheur.techno_lib.lang.LanguageGenProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
 
 /**
  * Lang generator. Specify with the function what type of translation you want then set the parameters to define the key.
@@ -24,6 +30,7 @@ public class ModLangGen extends LanguageGenProvider {
         addTooltipCredit("zombie_arm")
                 .en_us("Made by Jema")
                 .fr_fr("Créer par Jema");
+
 
         // tooltips catalyzers
         addTooltip("catalyzer-wind_charged")
@@ -44,6 +51,7 @@ public class ModLangGen extends LanguageGenProvider {
         addTooltip("catalyzer-diamond")
                 .en_us("This catalyzer can handle advanced incubations.")
                 .fr_fr("Ce catalyseur peut gérer des incubations avancées.");
+
 
         // tooltip
         addTooltip("rotten_item")
@@ -76,6 +84,7 @@ public class ModLangGen extends LanguageGenProvider {
         addTooltip("alloyed_sword")
                 .en_us("Useless like this, but needed to craft the best sword ever!")
                 .fr_fr("Inutile comme sa, mais nécessaire pour fabriquer la mailleur épée !");
+
 
         // sounds
         addSound("the_world_of_cubes")
@@ -127,6 +136,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Playing Ballad of the blocks")
                 .fr_fr("En train de jouer : Ballad of the blocks");
 
+
         // recipes
         addRecipeName("mysterious_magic")
                 .en_us("Mysterious magic")
@@ -147,6 +157,7 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Atomical stabilization")
                 .fr_fr("Stabilisation atomique");
 
+
         // menu (modpack)
         addMenuName("discord")
                 .en_us("Our discord")
@@ -162,10 +173,12 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("Mysterious magic")
                 .fr_fr("Magie mystérieuse");
 
+
         // villager
         addVillagerType("soundmaster")
                 .en_us("Sound master")
                 .fr_fr("Ingénieur son");
+
 
         // effects
         addEffect(ModEffects.ICED)
@@ -174,6 +187,7 @@ public class ModLangGen extends LanguageGenProvider {
         addEffect(ModEffects.QUANTUM_CHOCKED)
                 .en_us("Quantum chocked")
                 .fr_fr("Choc quantique");
+
 
         // death
         addAttackDeath(ModDamageTypes.BRAIN_FREEZE_ID.getPath())
@@ -194,6 +208,7 @@ public class ModLangGen extends LanguageGenProvider {
         addAttackDeathPlayer(ModDamageTypes.ELECTROCUTED_ID.getPath())
                 .en_us("%1$s got electrocuted whilst trying to escape %2$s")
                 .fr_fr("%1$s a été éléctrocuté en essayant de fuir %2$s");
+
 
         // creative mod tab
         addCreativeTab(ModCreativeModTabs.BALLS.get())
@@ -217,6 +232,7 @@ public class ModLangGen extends LanguageGenProvider {
         addCreativeTab(ModCreativeModTabs.MUSIC_DISCS.get())
                 .en_us("Music discs")
                 .fr_fr("Disques de musiques");
+
 
         // blocks
         addBlock(ModBlocks.ATOMICAL_STABILIZATOR)
@@ -494,7 +510,250 @@ public class ModLangGen extends LanguageGenProvider {
                 .en_us("And hard too")
                 .fr_fr("Solide aussi");
 
+
+        // items
+        addItem(ModItems.ACCUMULATION_ASSISTED_CIRCUIT_BOARD)
+                .en_us("Accumulation assisted circuit board")
+                .fr_fr("Circuit imprimé d'accumulation assisté");
+        addItem(ModItems.ALLOYED_SWORD)
+                .en_us("Alloyed sword")
+                .fr_fr("Epée-alliage");
+        addItem(ModItems.BALLAD_OF_THE_BLOCKS_MUSIC_DISC)
+                .en_us("Ballad of the blocks music disc")
+                .fr_fr("Disque de musique de Ballad of the blocks");
+        addRecordDesc(ModItems.BALLAD_OF_THE_BLOCKS_MUSIC_DISC)
+                .en_us("Ballad of blocks - Catoutou")
+                .fr_fr("Ballad of blocks - Catoutou");
+        addItem(ModItems.BAR_BRAWL_MUSIC_DISC)
+                .en_us("Bar brawl music disc")
+                .fr_fr("Disque de musique de Bar brawl");
+        addRecordDesc(ModItems.BAR_BRAWL_MUSIC_DISC)
+                .en_us("Bryan Tech - Bar brawl (CC0)")
+                .fr_fr("Bryan Tech - Bar brawl (CC0)");
+        addItem(ModItems.BASE_EGG)
+                .en_us("Egg base")
+                .fr_fr("Base d'oeuf");
+        addItem(ModItems.BASE_OF_DISC)
+                .en_us("Base of disc")
+                .fr_fr("Base de disque");
+        addItem(ModItems.BIOMASS)
+                .en_us("Biomass")
+                .fr_fr("Biomasse");
+        addItem(ModItems.BLUE_BIOMASS)
+                .en_us("Blue biomass")
+                .fr_fr("Biomasse bleu");
+        addItem(ModItems.DARKENED_BIOMASS)
+                .en_us("Darkened biomass")
+                .fr_fr("Biomasse assombrie");
+        addItem(ModItems.BLOCK_BY_BLOCK_MUSIC_DISC)
+                .en_us("Block by block music disc")
+                .fr_fr("Disque de musique de Block by block");
+        addRecordDesc(ModItems.BLOCK_BY_BLOCK_MUSIC_DISC)
+                .en_us("Block by block - Catoutou")
+                .fr_fr("Block by block - Catoutou");
+        addItem(ModItems.BUCKET_OF_LIQUID_CHOCOLATE_ICE_CREAM)
+                .en_us("Bucket of liquid chocolate ice cream")
+                .fr_fr("Seau de glace au chocolat liquide");
+        addItem(ModItems.BUCKET_OF_LIQUID_ICE_CREAM)
+                .en_us("Bucket of liquid ice cream")
+                .fr_fr("Seau de glace liquide");
+        addItem(ModItems.BUCKET_OF_LIQUID_STRAWBERRIES_ICE_CREAM)
+                .en_us("Bucket of liquid strawberry ice cream")
+                .fr_fr("Seau de glace à la fraise liquide");
+        addItem(ModItems.BUCKET_OF_LIQUID_SWEET_BERRIES_ICE_CREAM)
+                .en_us("Bucket of liquid sweet berries ice cream")
+                .fr_fr("Seau de glace aux baies sucrées liquide");
+        addItem(ModItems.BUCKET_OF_LIQUID_VANILLA_ICE_CREAM)
+                .en_us("Bucket of liquid vanilla ice cream")
+                .fr_fr("Seau de glace à la vanille liquide");
+        addItem(ModItems.BURGER)
+                .en_us("Burger")
+                .fr_fr("Hamburger");
+        addItem(ModItems.CALCULATION_ASSISTED_CIRCUIT_BOARD)
+                .en_us("Calculation assisted circuit board")
+                .fr_fr("Circuit imprimé de calcul assisté");
+        addItem(ModItems.CHOCOLATE_ICE_CREAM)
+                .en_us("Chocolate ice cream")
+                .fr_fr("Glace au chocolat");
+        addItem(ModItems.CHOCOLATE_SCOOP)
+                .en_us("Chocolate scoop")
+                .fr_fr("Boule de glace au chocolat");
+        addItem(ModItems.CHOCOLATE_SNOWBALL)
+                .en_us("Chocolate snowball")
+                .fr_fr("Boule de neige au chocolat");
+        addItem(ModItems.CHOCOLATE_SORBET)
+                .en_us("Chocolate sorbet")
+                .fr_fr("Sorbet au chocolat");
+        addItem(ModItems.CONE)
+                .en_us("Cone")
+                .fr_fr("Cône");
+        addItem(ModItems.CONTROL_PANEL)
+                .en_us("Control panel")
+                .fr_fr("Panneau de contrôle");
+        addItem(ModItems.CONTROLLED_STICK)
+                .en_us("Controlled stick")
+                .fr_fr("Baton de contrôle");
+        addItem(ModItems.COPPER_BALL)
+                .en_us("Copper ball")
+                .fr_fr("Balle de cuivre");
+        addItem(ModItems.DIAMOND_BALL)
+                .en_us("Diamond ball")
+                .fr_fr("Balle de diamant");
+        addItem(ModItems.GOLD_BALL)
+                .en_us("Gold ball")
+                .fr_fr("Balle d'or");
+        addItem(ModItems.IRON_BALL)
+                .en_us("Iron ball")
+                .fr_fr("Balle de fer");
+        addItem(ModItems.SAPPHIRE_BALL)
+                .en_us("Sapphire ball")
+                .fr_fr("Balle de saphir");
+        addItem(ModItems.SUPER_CHARGED_BALL)
+                .en_us("Super charged ball")
+                .fr_fr("Balle super chargée");
+        addItem(ModItems.DIAMOND_CATALYZER)
+                .en_us("Diamond catalyzer")
+                .fr_fr("Catalyseur en diamant");
+        addItem(ModItems.GOLD_CATALYZER)
+                .en_us("Gold catalyzer")
+                .fr_fr("Catalyseur d'or");
+        addItem(ModItems.IRON_CATALYZER)
+                .en_us("Iron catalyzer")
+                .fr_fr("Catalyseur de fer");
+        addItem(ModItems.NETHERITE_CATALYZER)
+                .en_us("Netherite catalyzer")
+                .fr_fr("Catalyseur de netherite");
+        addItem(ModItems.SUPER_CHARGED_CATALYZER)
+                .en_us("Super charged catalyzer")
+                .fr_fr("Catalyseur super chargé");
+        addItem(ModItems.WIND_CHARGED_CATALYZER)
+                .en_us("Wind charged catalyzer")
+                .fr_fr("Catalyseur chargé en vent");
+        addItem(ModItems.UNFINISHED_BLAZE_POWDER)
+                .en_us("Unfinished blaze powder")
+                .fr_fr("Poudre de blaze en cours de fabrication");
+        addItem(ModItems.UNFINISHED_EMERALD)
+                .en_us("Unfinished emerald")
+                .fr_fr("Émeraude en cours de fabrication");
+        addItem(ModItems.UNFINISHED_STORM_FRAGMENT)
+                .en_us("Unfinished storm fragment")
+                .fr_fr("Fragment de tempête en cours de fabrication");
+        addItem(ModItems.UNPROCESSED_QUANTUM_CORE)
+                .en_us("Unfinished quantum core")
+                .fr_fr("Noyau quantique en cours de fabrication");
+        addItem(ModItems.UNPROCESSED_SEA_PICKLE)
+                .en_us("Unfinished sea pickle")
+                .fr_fr("Concombre de mer en cours de fabrication");
+        addItem(ModItems.VANILLA_EXTRACT)
+                .en_us("Vanilla extract")
+                .fr_fr("Extrait de vanille");
+        addItem(ModItems.VANILLA_ICE_CREAM)
+                .en_us("Vanilla ice cream")
+                .fr_fr("Glace à la vanille");
+        addItem(ModItems.VANILLA_POD)
+                .en_us("Vanilla pod")
+                .fr_fr("Gousse de vanille");
+        addItem(ModItems.DRIED_VANILLA_POD)
+                .en_us("Dried vanilla pod")
+                .fr_fr("Gousse de vanille sèche");
+        addItem(ModItems.VANILLA_SCOOP)
+                .en_us("Vanilla scoop")
+                .fr_fr("Boule de glace à la vanille");
+        addItem(ModItems.VANILLA_SNOWBALL)
+                .en_us("Vanilla snowball")
+                .fr_fr("Boule de neige à la vanille");
+        addItem(ModItems.VANILLA_SORBET)
+                .en_us("Vanilla sorbet")
+                .fr_fr("Sorbet à la vanille");
+        addItem(ModItems.WET_DIRT)
+                .en_us("Wet dirt")
+                .fr_fr("Terre mouillé");
+        addItem(ModItems.WIND_CHARGED_INGOT)
+                .en_us("Wind charged ingot")
+                .fr_fr("Lingot chargé de vent");
+        addItem(ModItems.WIND_STICK)
+                .en_us("Wind stick")
+                .fr_fr("Baton de vent");
+        addItem(ModItems.YEAST)
+                .en_us("Yeast")
+                .fr_fr("Levure");
+        addItem(ModItems.ZOMBIE_ARM)
+                .en_us("Zombie arm")
+                .fr_fr("Bras de zombie");
+        addItem(ModItems.XP_RUNE)
+                .en_us("Xp rune")
+                .fr_fr("Rune d'xp");
+        addItem(ModItems.SPRUCE_RUNE)
+                .en_us("Spruce rune")
+                .fr_fr("Rune de sapin");
+        addItem(ModItems.OAK_RUNE)
+                .en_us("Oak rune")
+                .fr_fr("Rune de chêne");
+        addItem(ModItems.MONEY_RUNE)
+                .en_us("Money rune")
+                .fr_fr("Rune d'argent");
+        addItem(ModItems.EMPTY_RUNE)
+                .en_us("Empty rune")
+                .fr_fr("Rune vide");
+        addItem(ModItems.DICE)
+                .en_us("Dice")
+                .fr_fr("Dée");
+        addItem(ModItems.EXPLODED_POTATO)
+                .en_us("Exploded potato")
+                .fr_fr("Patate explosée");
+        addItem(ModItems.FIRE_DIAMOND)
+                .en_us("Fire diamond")
+                .fr_fr("Diamant de feu");
+        addItem(ModItems.FIRE_SEEDS)
+                .en_us("Fire seeds")
+                .fr_fr("Graines de feu");
+        addItem(ModItems.FIRE_STICK)
+                .en_us("Fire stick")
+                .fr_fr("Baton de feu");
+        addItem(ModItems.FIRE_SWORD)
+                .en_us("Fire sword")
+                .fr_fr("Epée de feu");
+        addItem(ModItems.DIRTY_HUMID_POTION)
+                .en_us("Dirty humid potion")
+                .fr_fr("Potion humide salle");
+        addItem(ModItems.EMPTY_BIG_FLASK)
+                .en_us("Empty big flask")
+                .fr_fr("Grande fiole vide");
+        addItem(ModItems.EMPTY_LITTLE_FLASK)
+                .en_us("Empty little flask")
+                .fr_fr("Petite fiole vide");
+        addItem(ModItems.EXTREMELY_DRY_DIRT)
+                .en_us("Extremely dry dirt")
+                .fr_fr("Terre extrêmement sèche");
+        addItem(ModItems.END_OF_THE_START_MUSIC_DISC)
+                .en_us("End of the start music disc")
+                .fr_fr("Disque de musique de End of the start");
+        addRecordDesc(ModItems.END_OF_THE_START_MUSIC_DISC)
+                .en_us("Destabilized - PunchDeck")
+                .fr_fr("Destabilized - PunchDeck");
+        addItem(ModItems.ENGINEERING_ASSISTED_CIRCUIT_BOARD)
+                .en_us("Engineering assisted circuit board")
+                .fr_fr("Circuit imprimé d'ingénierie assisté");
+        addItem(ModItems.LOGIC_ASSISTED_CIRCUIT_BOARD)
+                .en_us("Logic assisted circuit board")
+                .fr_fr("Circuit imprimé de logique assisté");
+        addItem(ModItems.QUANTUM_ASSISTED_CIRCUIT_BOARD)
+                .en_us("Quantum assisted circuit board")
+                .fr_fr("Circuit imprimé quantique assisté");
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
 
     public TranslationBuilder addMenuName(String id) {
         return add("menu." + id);
