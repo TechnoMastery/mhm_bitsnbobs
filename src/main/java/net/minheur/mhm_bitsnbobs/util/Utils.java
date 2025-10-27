ea901edb5a6c8e8a57d2be5201a3a1d6e9ac293b
Minheur4000 <ordi2526883@educ.normandie.fr>
2025-10-27 19:39:47 +0100

    added utility method

---
+++ src/main/java/net/minheur/mhm_bitsnbobs/util/Utils.java
@@ -0,0 +1,9 @@
+package net.minheur.mhm_bitsnbobs.util;
+
+import net.minheur.techno_lib.builders.RecipeJsonObjectBuilder;
+
+public class Utils {
+    public static RecipeJsonObjectBuilder json() {
+        return new RecipeJsonObjectBuilder();
+    }
+}
